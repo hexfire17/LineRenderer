1f946ccb176b16f7301fe9b24ea80f81aaa6e7c3
hexfire17 <hexfire17@gmail.com>
2016-12-13 18:25:15 -0500

    workingline

--- Assets/scripts/Artist.cs
+++ Assets/scripts/Artist.cs
@@ -6,12 +6,43 @@ public class Artist : MonoBehaviour
 	void Start()
 	{
 		_isDrawing = false;
-		Vector3 start = new Vector3 (0, 0, 0);
-		Vector3 end = new Vector3 (2, 2, 0);
+		drawLine (new Vector3(0, 0, 0), new Vector3 (1, 1, 0));
+		drawLine (new Vector3(-3, 3, 0), new Vector3 (0, 3, 0));
+
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			_isDrawing = true;
+			_lastPenLocation = _camera.ScreenToWorldPoint (Input.mousePosition);
+
+		}
+		else if (Input.GetMouseButtonUp (0))
+		{
+			_isDrawing = false;
+			Vector3 currentPenLocation = _camera.ScreenToWorldPoint (Input.mousePosition);
+
+			drawLine (_lastPenLocation, currentPenLocation);
+		}
+		else if (_isDrawing)
+		{
+
+		}
+	}
+
+	private void drawLine(Vector3 start, Vector3 end)
+	{
+		// change them to be in 2d space
+		start = new Vector3(start.x, start.y, 0);
+		end = new Vector3 (end.x, end.y, 0);
+
+		// debug anchors
 		Instantiate (_pen, start, _pen.transform.rotation);
 		Instantiate (_pen, end, _pen.transform.rotation);
 
-		// set position
+		// instantiate and set position
 		Vector3 linePosition = (start + end) / 2;
 		print ("position: " + linePosition);
 		GameObject line = Instantiate (_pen, linePosition, _pen.transform.rotation) as GameObject;
@@ -21,50 +52,14 @@ public class Artist : MonoBehaviour
 		print ("dist: " + distance);
 		Vector3 originalScale = line.transform.localScale;
 		print ("localScale: " + line.transform.localScale);
-		line.transform.localScale += (Vector3.forward * distance);
+		line.transform.localScale += (Vector3.right * distance);
 
-		// rotation
-		line.transform.rotation = Quaternion.LookRotation (end);
-	}
-
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown (0))
-		{
-			_isDrawing = true;
-
-			Vector3 currentPenLocation = _camera.ScreenToWorldPoint (Input.mousePosition);
-			currentPenLocation.z = 0;
-			Instantiate (_pen, currentPenLocation, _pen.transform.rotation);
-
-			_lastPenLocation = currentPenLocation;
-
-		}
-		else if (Input.GetMouseButtonUp (0))
-		{
-			_isDrawing = false;
-
-			Vector3 currentPenLocation = _camera.ScreenToWorldPoint (Input.mousePosition);
-			currentPenLocation.z = 0;
-
-			GameObject line = Instantiate (_pen, currentPenLocation, _pen.transform.rotation) as GameObject;
-
-			float distance = Vector3.Distance (currentPenLocation, _lastPenLocation);
-			Vector3 originalScale = line.transform.localScale;
-			Vector3 newScale = originalScale + (Vector3.right * distance);
-			line.transform.localScale = newScale;
-			line.transform.position = line.transform.position - (Vector3.right * distance / 2);
-
-			// rotation
-			float rotationAngle = Vector3.Angle (_lastPenLocation, currentPenLocation);
-			print(rotationAngle);
-			Vector3 rotationVector = new Vector3 (0, 0, rotationAngle);
-			line.transform.rotation = Quaternion.Euler (rotationVector.x, rotationVector.y, rotationVector.z);
-		}
-		else if (_isDrawing)
-		{
-
-		}
+		// set rotation
+		float slope = (end.y - start.y) / (end.x - start.x);
+		float angle = Mathf.Rad2Deg * Mathf.Atan (slope);
+		print ("slope: " + slope);
+		print ("FAngle: " + angle);
+		line.transform.rotation = Quaternion.Euler (0, 0, angle);
 	}
 
 	public GameObject _pen;
