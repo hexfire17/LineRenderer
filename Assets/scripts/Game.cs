6b22c62b7d5e2a518062fa11efa06f42d329cb0e
hexfire17 <hexfire17@gmail.com>
2016-12-12 19:23:59 -0500

    checkpoint, slide working

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () 
+	{
+	//	_levelGenerator = FindObjectOfType<LevelGenerator> ();
+	//	_levelGenerator._levelIndex = 0;
+	//	NextLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	//	if (Input.GetKeyDown (KeyCode.Return)) {
+	//		NextLevel ();
+	//	}
+	}
+
+	void NextLevel ()
+	{
+	//	_levelGenerator.GenerateLevel ();
+	//	_levelGenerator._levelIndex++;
+	}
+
+	//LevelGenerator _levelGenerator;
+}
---
+++ Assets/scripts/Rider.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rider : MonoBehaviour 
+{
+	void Start () 
+	{
+
+	}
+
+	void Update ()
+	{
+		// if dead? or make that an event subscription,... on collision??
+	}
+
+}
